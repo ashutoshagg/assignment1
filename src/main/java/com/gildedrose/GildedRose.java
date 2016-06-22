db006c6a9106028340c40b8648f1d471f9dd296f
ashtoshagg <ashutosh.aggarwal4@gmail.com>
2016-06-22 13:05:36 +0530

    added item to list

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,23 +11,15 @@ class GildedRose {
 		for (int i = 0; i < items.length; i++) {
 			if (!items[i].name.equals("Aged Brie")
 					&& !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-				if (items[i].quality > 0) {
-
-					
-					
+				if (items[i].quality > 0) {	
 					if(items[i].quality > 10 && items[i].name.equals("Sulfuras New"))
-					{
-						System.out.println(items[i].name);
+					{						
 						items[i].quality = items[i].quality - 1;
 					}					
 
 					else if (!items[i].name.equals("Sulfuras, Hand of Ragnaros") && !items[i].name.equals("Sulfuras New")) {
 						items[i].quality = items[i].quality - 1;
-					}
-					else{
-						System.out.println("hello");
-					}
-
+					}					
 
 
 				}
@@ -58,19 +50,12 @@ class GildedRose {
 			if (items[i].sellIn < 0) {
 				if (!items[i].name.equals("Aged Brie")) {
 					if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-						if (items[i].quality > 0) {
-						/*	if(items[i].quality > 10 && items[i].name.equals("Sulfuras New"))
-							{
-								System.out.println(items[i].name);
-								items[i].quality = items[i].quality - 1;
-							}	*/
-							if (!items[i].name.equals("Sulfuras, Hand of Ragnaros") && !items[i].name.equals("Sulfuras New")) {
-								System.out.println("---" + items[i].name);
+						if (items[i].quality > 0) {						
+							if (!items[i].name.equals("Sulfuras, Hand of Ragnaros") && !items[i].name.equals("Sulfuras New")) {								
 								items[i].quality = items[i].quality - 1;
 							}
 						}
 					} else {
-						System.out.println("-******-" + items[i].name);
 						items[i].quality = items[i].quality - items[i].quality;
 					}
 				} else {
